b70d52a0cc3b406edde428365d4c77ce2cdb7eab
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-08-21 12:41:21 -0300

    feat(class2) add gio's exercise

---
+++ GrupitoReactivo/src/class2/Gio/Content.tsx
@@ -0,0 +1,15 @@
+import Saludo from "./Saludo.tsx"
+import Perfil from "./Perfil.tsx";
+import "./class2.css"
+
+function Contenedor() {
+    
+    return(
+        <div>
+            <Saludo/>
+            <Perfil/>
+        </div>
+    )
+}
+
+export default Contenedor;
