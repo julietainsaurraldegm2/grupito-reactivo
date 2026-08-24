b380e288dd524911e92ac8addbe6a76009319325
julietainsaurraldegm2 <julieta.insaurralde@gm2dev.com>
2026-08-24 12:12:52 -0300

    feat/add  Juli's Tarjeta component and update App to include it

--- GrupitoReact/src/App.tsx
+++ GrupitoReact/src/App.tsx
@@ -1,11 +1,15 @@
 import Saludo from './class3/Juli/Saludo'
 import './App.css'
+import Tarjeta from './class3/Juli/TarjetaProducto'
 
 function App() {
   return (
-
+<>
 <Saludo nombre= "Jose" />
-    
+<Tarjeta nombre = "macucas" precio = {1000} stock = {12000}/>
+<Tarjeta nombre = "talitas" precio = {900} stock = {500}/>
+
+</>
      )
 }
 
